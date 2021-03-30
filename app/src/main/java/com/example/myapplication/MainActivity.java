b4f786b83407f53c9f63c806311311ee3b8c07b9
ests <aymankaddioui@gmail.com>
2021-03-30 15:14:51 +0100

    test

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void petit(View view) {
+    }
+
+    public void dejeuner(View view) {
+    }
+
+    public void diner(View view) {
+    }
+}
